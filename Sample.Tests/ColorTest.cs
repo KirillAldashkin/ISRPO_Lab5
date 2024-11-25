5ad42efea7221c51ed1a56bfe90333b8bfc4911d
KirillAldashkin <a89086520092k@yandex.ru>
2024-11-26 00:33:08 +0300

    Moved to NUnit

--- Sample.Tests/ColorTest.cs
+++ Sample.Tests/ColorTest.cs
@@ -1,71 +1,58 @@
 ﻿using Sample.Core;
 
-namespace Sample.Tests;
+namespace Test;
 
-[TestClass]
-public sealed class ColorTest
+public class ColorTest
 {
-    static void AssertNoThrow(Func<object?> func)
-    {
-        try
-        {
-            _ = func();
-        }
-        catch (Exception ex)
-        {
-            Assert.Fail("Expected to not throw anything, thrown {}", ex);
-        }
-    }
-
-    [TestMethod]
-    public void Test_FromHSV_Bounds()
+    [Test]
+    public void Test_FromHSV_Bounds() => Assert.Multiple(() =>
     {
         float dec0 = float.BitDecrement(0);
         float inc1 = float.BitIncrement(1);
 
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => Color.FromHSV(dec0, 0.5f, 0.5f));
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => Color.FromHSV(MathF.Tau, 0.5f, 0.5f));
-        AssertNoThrow(() => Color.FromHSV(float.BitDecrement(MathF.Tau), 0.5f, 0.5f));
-        AssertNoThrow(() => Color.FromHSV(0, 0.5f, 0.5f));
+        Assert.Throws<ArgumentOutOfRangeException>(() => Color.FromHSV(dec0, 0.5f, 0.5f));
+        Assert.Throws<ArgumentOutOfRangeException>(() => Color.FromHSV(MathF.Tau, 0.5f, 0.5f));
+        Assert.DoesNotThrow(() => Color.FromHSV(float.BitDecrement(MathF.Tau), 0.5f, 0.5f));
+        Assert.DoesNotThrow(() => Color.FromHSV(0, 0.5f, 0.5f));
 
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => Color.FromHSV(1, dec0, 0.5f));
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => Color.FromHSV(1, inc1, 0.5f));
-        AssertNoThrow(() => Color.FromHSV(1, 0, 0.5f));
-        AssertNoThrow(() => Color.FromHSV(1, 1, 0.5f));
+        Assert.Throws<ArgumentOutOfRangeException>(() => Color.FromHSV(1, dec0, 0.5f));
+        Assert.Throws<ArgumentOutOfRangeException>(() => Color.FromHSV(1, inc1, 0.5f));
+        Assert.DoesNotThrow(() => Color.FromHSV(1, 0, 0.5f));
+        Assert.DoesNotThrow(() => Color.FromHSV(1, 1, 0.5f));
 
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => Color.FromHSV(1, 0.5f, dec0));
-        Assert.ThrowsException<ArgumentOutOfRangeException>(() => Color.FromHSV(1, 0.5f, inc1));
-        AssertNoThrow(() => Color.FromHSV(1, 0.5f, 0));
-        AssertNoThrow(() => Color.FromHSV(1, 0.5f, 1));
-    }
+        Assert.Throws<ArgumentOutOfRangeException>(() => Color.FromHSV(1, 0.5f, dec0));
+        Assert.Throws<ArgumentOutOfRangeException>(() => Color.FromHSV(1, 0.5f, inc1));
+        Assert.DoesNotThrow(() => Color.FromHSV(1, 0.5f, 0));
+        Assert.DoesNotThrow(() => Color.FromHSV(1, 0.5f, 1));
+    });
 
-    [TestMethod]
-    public void Test_FromHSV_ZeroValue()
+    [Test]
+    public void Test_FromHSV_ZeroValue() => Assert.Multiple(() =>
     {
-        for(float h = 0; h < 6; h += 0.2f)
+        for (float h = 0; h < 6; h += 0.2f)
             for (float s = 0; s <= 1; s += 0.1f)
-                Assert.AreEqual(Color.FromHSV(h, s, 0), new(0, 0, 0));
-    }
+                Assert.That(Color.FromHSV(h, s, 0), Is.EqualTo(new Color(0, 0, 0)));
+    });
 
-    [TestMethod]
-    public void Test_FromHSV_ZeroSaturation()
+    [Test]
+    public void Test_FromHSV_ZeroSaturation() => Assert.Multiple(() =>
     {
         for (float h = 0; h < 6; h += 0.2f)
         {
-            Assert.AreEqual(Color.FromHSV(h, 0, 0.00f), new(0, 0, 0));
-            Assert.AreEqual(Color.FromHSV(h, 0, 0.25f), new(63, 63, 63));
-            Assert.AreEqual(Color.FromHSV(h, 0, 0.50f), new(127, 127, 127));
-            Assert.AreEqual(Color.FromHSV(h, 0, 0.75f), new(191, 191, 191));
-            Assert.AreEqual(Color.FromHSV(h, 0, 1.00f), new(255, 255, 255));
+            Assert.That(Color.FromHSV(h, 0, 0.00f), Is.EqualTo(new Color(0, 0, 0)));
+            Assert.That(Color.FromHSV(h, 0, 0.25f), Is.EqualTo(new Color(63, 63, 63)));
+            Assert.That(Color.FromHSV(h, 0, 0.50f), Is.EqualTo(new Color(127, 127, 127)));
+            Assert.That(Color.FromHSV(h, 0, 0.75f), Is.EqualTo(new Color(191, 191, 191)));
+            Assert.That(Color.FromHSV(h, 0, 1.00f), Is.EqualTo(new Color(255, 255, 255)));
         }
-    }
+    });
 
-    [TestMethod]
-    public void Test_FromHSV_Generic()
+    [Test]
+    public void Test_FromHSV_Generic() => Assert.Multiple(() =>
     {
-        Assert.AreEqual(Color.FromHSV(0, 1, 1), new(255, 0, 0));
-        Assert.AreEqual(Color.FromHSV(MathF.Tau * 5 / 12, 1, 0.5f), new(0, 127, 63));
-        Assert.AreEqual(Color.FromHSV(MathF.Tau * 5 / 6, 0.25f, 0.75f), new(191, 143, 191));
-        Assert.AreEqual(Color.FromHSV(MathF.Tau / 18, 0.75f, 0.5f), new(127, 63, 31));
-    }
+        Assert.That(Color.FromHSV(0, 1, 1), Is.EqualTo(new Color(255, 0, 0)));
+        Assert.That(Color.FromHSV(MathF.Tau * 5 / 12, 1, 0.5f), Is.EqualTo(new Color(0, 127, 63)));
+        Assert.That(Color.FromHSV(MathF.Tau * 5 / 6, 0.25f, 0.75f), Is.EqualTo(new Color(191, 143, 191)));
+        Assert.That(Color.FromHSV(MathF.Tau / 18, 0.75f, 0.5f), Is.EqualTo(new Color(127, 63, 31)));
+    });
 }
